133192ff9ad8269b08591bc2b8fbbe096ca1d8c3
Nick Rodriguez <nicholasrodri92@gmail.com>
2020-10-18 23:23:46 -0500

    added transform function for keys when pressed

--- Day1/drumKeys.js
+++ Day1/drumKeys.js
@@ -3,9 +3,21 @@ const keyA = document.getElementById('aMinor');
 window.addEventListener('keydown', function(e){
 	const audio = document.querySelector(`audio[class="${e.keyCode}"]`);
 	const key = document.querySelector(`.key`);
+	if(!audio) return;
 	audio.currentTime = 0;	
 	audio.play();
-	console.log(key);
+	key.classList.add('playing');
 });
+
+//remove transform
+function removeTransition(e) {
+	if(e.propertyName !== 'transform') return;
+
+	this.classList.remove('playing');
+}
+//transform when key is pressed
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 
